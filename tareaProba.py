71adf803ac20dbdb71dbca0edd7ed3cd4a3ea728
PCarrascoDev <pedro.carrascoc@mail.udp.cl>
2019-06-09 21:41:57 +0000

    Código comentado

--- tareaProba.py
+++ tareaProba.py
@@ -5,39 +5,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def randomString():
-    """Random String Generator"""
-    stringLength = random.randint(0, 100)
+    """Generador de strings aleatorios"""
+    stringLength = random.randint(1, 100)
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 def failRate(probPercent=7):
+    """Calcular la probabilidad de fallo para el envío de cada paquete"""
     decimalProb = probPercent / 100
-    return (random.random() < decimalProb)
+
+    #Retorna True en una probabilidad = (probPercent / 100), de lo contrario False
+    return (random.random() < decimalProb) 
 
 class Message():
+    """Clase Message, contiene un string aleatorio en self.data"""
     def __init__(self):
         self.data = randomString()
     def getData(self):
         return self.data
 
 class DataPacket():
+    """Clase DataPacket; cada paquete de datos contiene n mensajes.
+        *Por defecto n = 1"""
     def __init__(self):
         self.messages = []
     
     def appendMessage(self, message):
+        #Añadir un mensaje a la lista
         self.messages.append(message)
 
     def getMessages(self):
+        #Retornar los mensajes
         return self.messages
 
 class Receiver():
+    """Clase Receiver: encargado de recibir los paquetes de datos con mensajes."""
     def __init__(self):
         self.receivedPackets = []
     
     def receivePacket(self, packet):
+        #Recibir 1 paquete de datos
         self.receivedPackets.append(packet)
 
     def countMessages(self):
+        #Contar los mensajes en todos los paquetes de datos recibidos.
         msgCounter = 0
         for packet in self.receivedPackets:
             for message in packet.getMessages():
@@ -46,28 +57,41 @@ class Receiver():
         return msgCounter
 
 class WebServer():
+    """Clase WebServer: encargada de enviar los paquetes de datos al Receiver"""
     def __init__(self, messagesByPacket=1):
+        #Primero se generan 200 mensajes aleatorios
         self.messages = [Message() for x in range(200)]
         self.packets = []
+
+        #Luego se crean paquetes de datos para enviar los mensajes, dependiendo la cantidad de mensajes.
         packetNum = 0
         while(len(self.messages) > 0):
-
             self.packets.append(DataPacket())
+            
+            #Si se supera la cantidad de mensajes por paquete, se crea otro paquete para llenar
             for x in range(messagesByPacket):
                 if (len(self.messages) > 0):
+                    #Se añade el mensaje al paquete, y se saca de la lista de mensajes, siempre y cuando la lista no esté vacía
                     self.packets[packetNum].appendMessage(self.messages.pop())
+
+            #Si aún quedan mensajes, se siguen contando paquetes.
             if (len(self.messages) > 0):
                 packetNum = packetNum + 1
     
+
     def sendPackets(self, receiver=Receiver()):
+        #Esta función envía los paquetes a un receiver
         for packet in self.packets:
+            #El envío de cada paquete es dependiente de la probabilidad de failRate()
             if(failRate() is not True):
                 receiver.receivePacket(packet)
 
 class Simulation():
+    """Clase Simulation: se encarga de ejecutar la simulación n veces y luego plottearla en un histograma."""
+
     def __init__(self):
         self.Xvalues = []
-        for x in range(1000):
+        for x in range(10000):
             
             webServer = WebServer()
             receiver = Receiver()
@@ -76,6 +100,7 @@ class Simulation():
             self.Xvalues.append(200 - receiver.countMessages())
 
     def plotSim(self):
+        num_bins = 5
         n, bins, patches = plt.hist(self.Xvalues, num_bins, normed=1, facecolor='blue', alpha=0.5)
 
 if __name__ == "__main__":
